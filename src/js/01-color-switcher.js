98778c4e01e29db68cfe3ea471fc59e45b6ba48b
Dtimewarp <dtimewarp@gmail.com>
2023-11-23 14:16:59 +0200

    task 1 - done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const btnStart = document.querySelector("button[data-start]")
+const btnStop = document.querySelector("button[data-stop]")
+const delay = 1000
+let timerId = null
+
+
+btnStart.addEventListener("click", handleClick)
+btnStop.addEventListener("click", () => {
+    clearInterval(timerId);
+});
+
+function handleClick(click) {
+    btnStart.disabled = true;
+    timerId = setInterval(()=> {
+        const randomColor = getRandomHexColor(); 
+        document.body.style.backgroundColor = randomColor;;
+    }, delay);
+    
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
 
